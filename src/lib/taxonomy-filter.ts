11ea369d2aed5dce75c3e7624a47f869ffcb6cec
Shihab Mahamud <shihab4t@gmail.com>
2026-07-20 23:07:59 +0600

    wip

--- src/lib/taxonomy-filter.ts
+++ src/lib/taxonomy-filter.ts
@@ -1,4 +1,4 @@
-import { slugify } from '@/lib/utils/textConverter';
+import { slugify } from '@/lib/text-converter';
 
 const taxonomyFilter = (posts: any[], name: string, key: string) =>
     posts.filter((post) => post.data[name].map((name: string) => slugify(name)).includes(key));
